03edbb2e9923f8ba18da810242cd375720cdb295
ErikaPlugdj <erika.plugdj@gmail.com>
2016-07-26 18:04:59 +0200

    Update ArmyBot.js

--- ArmyBot.js
+++ ArmyBot.js
@@ -3284,7 +3284,42 @@ loveCommand: {
                     }
                 }
             },
-
+sklepCommand: {
+                 command: ['sklep', 'cellar'],
+                 rank: 'user',
+                 type: 'startsWith',
+                 getsklepovky: function (chat) {
+                     var c = Math.floor(Math.random() * basicBot.chat.sklepovky.length);
+                     return basicBot.chat.sklepovky[c];
+                 },
+                 functionality: function (chat, cmd) {
+                     if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                     if (!basicBot.commands.executable(this.rank, chat)) return void (0);
+                     else {
+                         var msg = chat.message;
+ 
+                         var space = msg.indexOf(' ');
+                         if (space === -1) {
+                             API.sendChat(basicBot.chat.givesklep);
+                             return false;
+                         }
+                         else {
+                             var name = msg.substring(space + 2);
+                             var user = basicBot.userUtilities.lookupUserName(name);
+                             if (user === false || !user.inRoom) {
+                                 return API.sendChat(subChat(basicBot.chat.nousersklep, {name: name}));
+                             }
+                             else if (user.username === chat.un) {
+                                 return API.sendChat(subChat(basicBot.chat.selfsklep, {name: name}));
+                             }
+                             else {
+                                 return API.sendChat(subChat(basicBot.chat.sklep, {nameto: user.username, namefrom: chat.un, SKLEPOVKY: this.getsklepovky()}));
+                             }
+                         }
+                     }
+                 }
+             },
+             
             skipCommand: {
                 command: ['skip', 'smartskip'],
                 rank: 'bouncer',
