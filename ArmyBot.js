1a0c469c86f1a8580d7ea64efec2e335296ff4cf
ErikaPlugdj <erika.plugdj@gmail.com>
2016-07-26 17:05:18 +0200

    Update ArmyBot.js

--- ArmyBot.js
+++ ArmyBot.js
@@ -2269,7 +2269,41 @@ fackaCommand: {
                      }
                  }
              },
-             
+             fuckCommand: {
+                 command: 'fuck',
+                 rank: 'user',
+                 type: 'startsWith',
+                 getfucks: function (chat) {
+                     var c = Math.floor(Math.random() * basicBot.chat.fucks.length);
+                     return basicBot.chat.fucks[c];
+                 },
+                 functionality: function (chat, cmd) {
+                     if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                     if (!basicBot.commands.executable(this.rank, chat)) return void (0);
+                     else {
+                         var msg = chat.message;
+ 
+                         var space = msg.indexOf(' ');
+                         if (space === -1) {
+                             API.sendChat(basicBot.chat.givefuck);
+                             return false;
+                         }
+                         else {
+                             var name = msg.substring(space + 2);
+                             var user = basicBot.userUtilities.lookupUserName(name);
+                             if (user === false || !user.inRoom) {
+                                 return API.sendChat(subChat(basicBot.chat.nouserfuck, {name: name}));
+                             }
+                             else if (user.username === chat.un) {
+                                 return API.sendChat(subChat(basicBot.chat.selffuck, {name: name}));
+                             }
+                             else {
+                                 return API.sendChat(subChat(basicBot.chat.fuck, {nameto: user.username, namefrom: chat.un, FUCKS: this.getfucks()}));
+                             }
+                         }
+                     }
+                 }
+             },
             fbCommand: {
                 command: 'fb',
                 rank: 'user',
