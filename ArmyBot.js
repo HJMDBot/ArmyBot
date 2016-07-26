636ca13ddff7fba567334e89cdaf378a5577d12b
ErikaPlugdj <erika.plugdj@gmail.com>
2016-07-26 16:37:45 +0200

    Update ArmyBot.js

--- ArmyBot.js
+++ ArmyBot.js
@@ -236,7 +236,7 @@
     var botCreatorIDs = ["3851534", "4105209"];
 
     var basicBot = {
-        version: "3.1.1",
+        version: "3.3.3",
         status: false,
         name: "ArmyBot",
         loggedInID: null,
@@ -2449,7 +2449,41 @@ fackaCommand: {
                     }
                 }
             },
-
+hugCommand: {
+                 command: 'hug',
+                 rank: 'user',
+                 type: 'startsWith',
+                 gethugs: function (chat) {
+                     var c = Math.floor(Math.random() * basicBot.chat.hugs.length);
+                     return basicBot.chat.hugs[c];
+                 },
+                 functionality: function (chat, cmd) {
+                     if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                     if (!basicBot.commands.executable(this.rank, chat)) return void (0);
+                     else {
+                         var msg = chat.message;
+ 
+                         var space = msg.indexOf(' ');
+                         if (space === -1) {
+                             API.sendChat(basicBot.chat.givehug);
+                             return false;
+                         }
+                         else {
+                             var name = msg.substring(space + 2);
+                             var user = basicBot.userUtilities.lookupUserName(name);
+                             if (user === false || !user.inRoom) {
+                                 return API.sendChat(subChat(basicBot.chat.nouserhug, {name: name}));
+                             }
+                             else if (user.username === chat.un) {
+                                 return API.sendChat(subChat(basicBot.chat.selfhug, {name: name}));
+                             }
+                             else {
+                                 return API.sendChat(subChat(basicBot.chat.hug, {nameto: user.username, namefrom: chat.un, HUGS: this.gethugs()}));
+                             }
+                         }
+                     }
+                 }
+             },
             helpCommand: {
                 command: 'help',
                 rank: 'user',
