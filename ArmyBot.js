5430358d2f3e585277f9925b851538f1fa9893ae
ErikaPlugdj <erika.plugdj@gmail.com>
2016-07-26 15:35:59 +0200

    Update ArmyBot.js

--- ArmyBot.js
+++ ArmyBot.js
@@ -1505,6 +1505,42 @@
                 },
              **/
 
+alkoholCommand: {
+                 command: ['alkohol', 'alcohol'],
+                 rank: 'user',
+                 type: 'startsWith',
+                 getALKOHOLY: function (chat) {
+                     var c = Math.floor(Math.random() * basicBot.chat.alkoholy.length);
+                     return basicBot.chat.alkoholy[c];
+                 },
+                 functionality: function (chat, cmd) {
+                     if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                     if (!basicBot.commands.executable(this.rank, chat)) return void (0);
+                     else {
+                         var msg = chat.message;
+ 
+                         var space = msg.indexOf(' ');
+                         if (space === -1) {
+                             API.sendChat(basicBot.chat.givealkohol);
+                             return false;
+                         }
+                         else {
+                             var name = msg.substring(space + 2);
+                             var user = basicBot.userUtilities.lookupUserName(name);
+                             if (user === false || !user.inRoom) {
+                                 return API.sendChat(subChat(basicBot.chat.nouseralkohol, {name: name}));
+                             }
+                             else if (user.username === chat.un) {
+                                 return API.sendChat(subChat(basicBot.chat.selfalkohol, {name: name}));
+                             }
+                             else {
+                                 return API.sendChat(subChat(basicBot.chat.alkohol, {nameto: user.username, namefrom: chat.un, ALKOHOLY: this.getALKOHOLY()}));
+                             }
+                         }
+                     }
+                 }
+             },
+
             activeCommand: {
                 command: 'active',
                 rank: 'bouncer',
