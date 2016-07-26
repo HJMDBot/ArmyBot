e7162537b6ff5b80a1d5d8d7038c979dd6bc87fb
ErikaPlugdj <erika.plugdj@gmail.com>
2016-07-26 18:15:07 +0200

    Update ArmyBot.js

--- ArmyBot.js
+++ ArmyBot.js
@@ -4068,7 +4068,19 @@ sklepCommand: {
                      }
                  }
              },
-
+zpetCommand: {
+                 command: 'zpet',
+                 rank: 'user',
+                 type: 'exact',
+                 functionality: function (chat, cmd) {
+                     if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                     if (!basicBot.commands.executable(this.rank, chat)) return void (0);
+                     else {
+                       API.sendChat(subChat(basicBot.chat.zpet, {name: chat.un}));  
+                     }
+                 }
+             },
+             
             youtubeCommand: {
                 command: 'youtube',
                 rank: 'user',
