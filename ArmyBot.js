3692755799b516ddb39bb7aad98fab4e451f1eaf
ErikaPlugdj <erika.plugdj@gmail.com>
2016-07-26 17:48:29 +0200

    Update ArmyBot.js

--- ArmyBot.js
+++ ArmyBot.js
@@ -3951,6 +3951,41 @@ loveCommand: {
                     }
                 }
             },
+             zvireCommand: {
+                 command: ['zvire', 'animal'],
+                 rank: 'user',
+                 type: 'startsWith',
+                 getanimal: function (chat) {
+                     var c = Math.floor(Math.random() * basicBot.chat.animal.length);
+                     return basicBot.chat.animal[c];
+                 },
+                 functionality: function (chat, cmd) {
+                     if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                     if (!basicBot.commands.executable(this.rank, chat)) return void (0);
+                     else {
+                         var msg = chat.message;
+ 
+                         var space = msg.indexOf(' ');
+                         if (space === -1) {
+                             API.sendChat(basicBot.chat.givezvire);
+                             return false;
+                         }
+                         else {
+                             var name = msg.substring(space + 2);
+                             var user = basicBot.userUtilities.lookupUserName(name);
+                             if (user === false || !user.inRoom) {
+                                 return API.sendChat(subChat(basicBot.chat.nouserzvire, {name: name}));
+                             }
+                             else if (user.username === chat.un) {
+                                 return API.sendChat(subChat(basicBot.chat.selfzvire, {name: name}));
+                             }
+                             else {
+                                 return API.sendChat(subChat(basicBot.chat.zvire, {nameto: user.username, namefrom: chat.un, ANIMAL: this.getanimal()}));
+                             }
+                         }
+                     }
+                 }
+             },
 
             youtubeCommand: {
                 command: 'youtube',
