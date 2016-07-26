28af74bf45e6a8f40ab62b04219eb7e75323b934
ErikaPlugdj <erika.plugdj@gmail.com>
2016-07-26 11:10:46 +0200

    Update ArmyBot.js

--- ArmyBot.js
+++ ArmyBot.js
@@ -309,9 +309,9 @@
             songstats: true,
             commandLiteral: "!",
             blacklists: {
-                NSFW: "https://rawgit.com/basicBot/custom/master/blacklists/NSFWlist.json",
-                OP: "https://rawgit.com/basicBot/custom/master/blacklists/OPlist.json",
-                BANNED: "https://rawgit.com/basicBot/custom/master/blacklists/BANNEDlist.json"
+                NSFW: "https://rawgit.com/ErikaPlugdj/custom/master/blacklist/NSFWlist.json",
+                OP: "https://rawgit.com/ErikaPlugdj/custom/master/blacklist/OPlist.json",
+                BANNED: "https://rawgit.com/ErikaPlugdj/custom/master/blacklist/BANNEDlist.json"
             }
         },
         room: {
