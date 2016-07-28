60d0108a808e5a794a7daccb132ddfcb203b1c98
ErikaPlugdj <erika.plugdj@gmail.com>
2016-07-28 08:59:59 +0200

    Update ArmyBot.js

--- ArmyBot.js
+++ ArmyBot.js
@@ -3174,6 +3174,19 @@ loveCommand: {
                 }
             },
             
+            prikazyCommand: {
+                 command: 'prikazy',
+                 rank: 'user',
+                 type: 'exact',
+                 functionality: function (chat, cmd) {
+                     if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                     if (!basicBot.commands.executable(this.rank, chat)) return void (0);
+                     else {
+                         API.sendChat(basicBot.chat.prikazy);
+                     }
+                 }
+             },
+             
             refreshCommand: {
                 command: 'refresh',
                 rank: 'manager',
